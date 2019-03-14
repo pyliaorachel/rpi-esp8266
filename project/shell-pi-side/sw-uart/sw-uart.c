af1cc5179caaf6d1f3848e32158eb53357f473df
Peiyu Liao <pyliao@stanford.edu>
2019-03-13 20:29:30 -0700

    Finish RX

--- project/shell-pi-side/sw-uart/sw-uart.c
+++ project/shell-pi-side/sw-uart/sw-uart.c
@@ -4,11 +4,11 @@
 #define RX_SOFT 6
 #define DELAY 104U // baudrate 9600
 
-unsigned RX_DATA = 0;
+unsigned char RX_DATA = 0;
 
 void int_handler(unsigned pc) {
   if (gpio_event_detected(RX_SOFT)) {
-    delay_us(DELAY >> 1);
+    delay_us(DELAY);
 
     // Latch 8 bits of data
     for (int i = 0; i < 8; i++) {
@@ -19,7 +19,7 @@ void int_handler(unsigned pc) {
     // End bit
     gpio_read(RX_SOFT);
 
-    uart_putc(RX_DATA);
+    printk("%c", RX_DATA);
     RX_DATA = 0;
 
     gpio_event_clear(RX_SOFT);
